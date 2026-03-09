d9df8b0b6e3e76031959835191f54f57da0febee
Thomas Hartwig <account@thomashartwig.com>
2026-03-09 23:24:17 +0100

    ✨ feat(cli): add Matter ASCII logo to version command

--- cli/version.go
+++ cli/version.go
@@ -11,6 +11,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const matterLogo = `
+        ░░█                                                                          
+        ░██░                                                                         
+      ░ ░██░ ░                                           █    ░█░                    
+    ░██████████░             ████░ ░████      █████ █  ░███████████   ░████      ░██ 
+   ░   ░░▓█░░   ░           █    ░█▓    █▒  ██░    ██    █    ░█░   ░█░    ░█   █░   
+   ███░      ░███           █    ░█░    ░█░ █       █    █    ░█░   ██████████ ░█░   
+    ░███    ███             █    ░█░    ░█░ █░      █    █    ░█░   ░█         ░█░   
+  ░██████  ░██████          █    ░█░    ░█░  ░███████    ███▒  ░███  ░██████░  ░█░   
+ ██▓   ██  ██░  ░██                                                                  
+        █  █░                                                                                                                                                                                                                                           
+`
+
 // newVersionCmd creates the `matter version` subcommand.
 func newVersionCmd() *cobra.Command {
 	return &cobra.Command{
@@ -18,6 +31,7 @@ func newVersionCmd() *cobra.Command {
 		Short: "Print version information",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			w := cmd.OutOrStdout()
+			fmt.Fprint(w, matterLogo)
 			fmt.Fprintf(w, "%s %s\n", output.Bold("matter"), output.Success(version))
 			fmt.Fprintf(w, "  %s  %s\n", output.Label("commit:"), output.Value(commit))
 			fmt.Fprintf(w, "  %s   %s\n", output.Label("built:"), output.Value(date))
